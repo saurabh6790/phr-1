b03a27c88bd6fcff0bf337223fec0cf89922dd52
Tejal <tejal.s@indictranstech.com>
2014-12-13 21:24:05 +0530

    created doctypes

---
+++ phr/phr/doctype/provider_category/test_provider_category.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2013, indictrans and Contributors
+# See license.txt
+
+import frappe
+import unittest
+
+test_records = frappe.get_test_records('Provider Category')
+
+class TestProviderCategory(unittest.TestCase):
+	pass
---
+++ phr/phr/doctype/share_files_log/test_share_files_log.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2013, indictrans and Contributors
+# See license.txt
+
+import frappe
+import unittest
+
+test_records = frappe.get_test_records('Share Files Log')
+
+class TestShareFilesLog(unittest.TestCase):
+	pass
