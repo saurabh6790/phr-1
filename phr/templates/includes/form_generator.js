fc882361fdb23a2f217be6c9b71014b7f5e0a2c4
saurabh <saurabh6790@gmail.com>
2014-12-23 22:32:09 +0530

    [oops][for form rendering]

--- phr/templates/includes/form_generator.js
+++ phr/templates/includes/form_generator.js
@@ -1,26 +1,30 @@
-frappe.RenderFormFields = Class.extend({
-	init:function(){
-		console.log("form render")
-		this.get_field_meta()
-	},
+var RenderFormFields = function(){
+	this.wrapper = ""
+}
+
+$.extend(RenderFormFields.prototype,{
+	init:function(wrapper){
+		this.wrapper = wrapper;
+		this.get_field_meta();
+	} ,
 	get_field_meta:function(){
 		var me = this;
 		frappe.call({
 			method:'phr.templates.pages.patient.get_data_to_render',
 			callback: function(r){
-				me.render_fields(fr.message[0], r.message[1])
+				me.render_fields(r.message[0], r.message[1])
 			}
 		})
 	},
 	render_fields:function(fields, values){
-		$content = $(this.document).find("#main-con")
 		h="";
 		for (i=0;i<fields.length;i++){
-			console.log(fields[i]["fieldtype"])
+			// console.log(fields[i]["fieldtype"])
 			if (fields[i]["fieldtype"]=="data"){
 				 h+='<input type="text" value="'+values[fields[i]["fieldname"]]+'"">'
 			}	
 		}
-		$($content).append(h)
+		$(this.wrapper).append(h)
+		// msgprint(__("Permanently Submit {0}?", ["test"]))
 	}
 })
