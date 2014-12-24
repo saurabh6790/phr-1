10ea7ec92c6c081cb4db9740aab54c5f45ae3182
pawaranand <pawaranand00@gmail.com>
2014-12-24 17:49:34 +0530

    pull

--- phr/templates/includes/patient.js
+++ phr/templates/includes/patient.js
@@ -12,14 +12,35 @@ $(document).ready(function () {
 			 h="";
 			 fields=r.message[0]
 			 values=r.message[1]
-			 for (i=0;i<fields.length;i++){
-			 	console.log(fields[i]["fieldtype"])
-			 	if (fields[i]["fieldtype"]=="data"){
-			 		 h+='<input type="text" value="'+values[fields[i]["fieldname"]]+'"">'
-			 	}	
+			 tab=r.message[2]
+			 var count=0
+			 if (tab==1){
+			 	h+='<div role="tabpanel"><ul class="nav nav-tabs tabui" role="tablist"></ul><div class="tab-content"></div>'
+			 	$($content).append(h)
+			 	var $li=$(document).find(".tabui")
+			 	var $tab-content=$(document).find(".tab-content")
 			 }
-			 $($content).append(h)
-			}
+			 for (i=0;i<fields.length;i++){
+			 	var value=(values[fields[i]["fieldname"]]? values[fields[i]["fieldname"]]:"");
+			 	if (tab==1){
+
+			 	 		if (fields[i]["fieldtype"]=="Section Break"){
+			 				$('<li role="presentation" class="active"><a href="#'+fields[i]["fieldname"]+'" aria-controls="home" role="tab" data-toggle="tab">'+fields[i]["fieldname"]+'</a></li>').appendTo($li)
+			 			}
+			 			if (fields[i]["fieldtype"]=="data"){
+			 				var tabdiv='<div><label>'+fields[i]["label"]+'</label><input class="form-control" style="width:30%" placeholder="'+fields[i]["label"]+'" value="'+value+'"></div>'
+			 			}
+
+			 		}
+			 		h +='<div class="form-group">'
+			 		if (fields[i]["fieldtype"]=="data"){
+			 			h+='<div><label>'+fields[i]["label"]+'</label><input class="form-control" style="width:30%" placeholder="'+fields[i]["label"]+'" value="'+value+'"></div>'
+			 		}
+			 	}
+			 		
+			 }
+			 
+			
 		})
 		/*$.getJSON(url,function(){
       		console.log("success")
--- phr/templates/pages/patient.py
+++ phr/templates/pages/patient.py
@@ -13,9 +13,10 @@ def get_data_to_render():
 	with open(os.path.join(os.path.dirname(__file__), "profile.json"), "r") as json_data:
 		json_data = json.loads(json_data.read())
 	if json_data:
-		data=json_data['data']
+		fields=json_data['fields']
+		tab=json_data['tab']
 		values=get_values()
-	return data,values
+		return fields,values,tab
 	
 def get_values():
 	values={"person_firstname": "Amit" ,"person_lastname":"Shukla" ,"gender":"Male" }
