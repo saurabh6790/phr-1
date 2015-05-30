1d68787b68b139124be54471e8e1a43ad34bd5d6
saurabh <saurabh6790@gmail.com>
2015-05-30 16:51:07 +0530

    advertisement notify2

--- phr/phr/doctype/advertisements/advertisements.py
+++ phr/phr/doctype/advertisements/advertisements.py
@@ -14,13 +14,49 @@ class Advertisements(Document):
 			"ad_description": self.ad_description,
 			"ad_link": self.ad_link
 		}]
-		
-		clsify_method(adv_data)
 
-def clsify_method():
-	pass
+		classify_method(adv_data)
+
+def classify_method(adv_data):
+	sms_list = []
+	emails_list = []
+	for adv in adv_data:
+		""" {"notify_to":[emails/numbers], "title": title, "description": description, "link": link} """
+		sms_list, emails_list = get_contact_details(adv)
+
+		if adv.get("notify_via") == 'Email':
+			send_adv_email({ 
+					"notify_to" : [email[0] for email in emails_list],
+					"title": adv.get('ad_title'),
+					"description": adv.get('ad_description'),
+					"link": adv.get('ad_link')
+				})
+
+		elif adv.get("notify_via") == 'SMS': 
+			send_adv_sms({
+					"notify_to" : [num[0] for num in sms_list],
+					"title": adv.get('ad_title'),
+					"description": adv.get('ad_description'),
+					"link": adv.get('ad_link')
+				})
+			
+		else:pass
+
+def get_contact_details(adv):
+	contact_list = frappe.db.sql("""select contact from tabUser where %(cond)s"""%{
+			"cond": get_cond(adv)
+		},as_list=1)
+
+	emails_list = frappe.db.sql("""select email from tabUser where %(cond)s"""%{
+			"cond": get_cond(adv)
+		},as_list=1)
+
+	return contact_list, emails_list
+
+def get_cond(adv):
+	return "access_type in ('Patient', 'Prvider')" if adv.get("notification_to") == 'Both' else "access_type = '%s'"%adv.get("notification_to")
+
 def send_adv_email():
 	pass
 def send_adv_sms():
 	pass
-
