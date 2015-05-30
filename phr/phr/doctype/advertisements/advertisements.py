97d49580d901a39dcd649f157a5f7f3782663c22
saurabh <saurabh6790@gmail.com>
2015-05-30 13:31:08 +0530

    advertisement notify-1

--- phr/phr/doctype/advertisements/advertisements.py
+++ phr/phr/doctype/advertisements/advertisements.py
@@ -6,4 +6,21 @@ import frappe
 from frappe.model.document import Document
 
 class Advertisements(Document):
+	def send_notification(self):
+		adv_data = [{
+			"notification_to": self.notification_to,
+			"notify_via": self.notify_via,
+			"ad_title": self.ad_title,
+			"ad_description": self.ad_description,
+			"ad_link": self.ad_link
+		}]
+		
+		clsify_method(adv_data)
+
+def clsify_method():
 	pass
+def send_adv_email():
+	pass
+def send_adv_sms():
+	pass
+
